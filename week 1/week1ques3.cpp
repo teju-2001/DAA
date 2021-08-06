15db95d1525795e38d97b9df65a14ca7ef209778
teju-2001 <tejaswinisingh2001@gmail.com>
2021-08-06 13:26:25 +0530

    Update week1ques3.cpp

--- week 1/week1ques3.cpp
+++ week 1/week1ques3.cpp
@@ -1,52 +1,43 @@
+
+/* jump search */
 #include<iostream>
-#include <bits/stdc++.h>
+#include<math.h>
 using namespace std;
-
-int main(){
-cout<<"Enter the number of test cases"<<endl;
-int t;
-cin>>t;
-int comp=0;
-while(t>0)
+int main()
 {
-int n,k,c;
-cout<<"Enter the value of n"<<endl;
-cin>>n;
-int a[n];
-cout<<"Enter the value of array"<<endl;
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    cout<<"Enter the value of k"<<endl;
-    cin>>k;
-    c=0;
-    int high = sqrt(n);
-    int low=0;
-    while(a[high]<=k&&high<n)
-    {
-        low=high;
-        high+=sqrt(n);
 
-
-    }
-    for(int i=low;i<high;i++)
-        { comp++;
-        if(a[i]==k)
+    int t;
+    cin>>t;
+    while(t>0)
+     {
+        int n,k,c=0;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+          cin>>a[i];
+        cin>>k;
+        int flag=0;
+        int s=sqrt(n);
+        int m = s,beg=0;
+       while(a[m]<=k&&m<n)
          {
-
-        cout<<"Present ";
-        cout<<comp<<endl;
-        c=1;
-        break;
+            c++;
+            beg=m;
+            m=m+s;
          }
-         }
-    if(c==0){
-        cout<<"Not Present ";
-        cout<<comp<<endl;
-
-    }
-       t--;//end of while loop of test cases
+       for(int i=beg;i<m;i++)
+        { 
+            c++;
+            if(a[i]==k)
+             {
+               cout<<"Present "<<c<<endl;
+               flag=1;
+               break;
+             }
+        }
+    if(flag==0)
+        cout<<"Not Present "<<c<<endl;
+    t--;
 }
 return 0;
 }
